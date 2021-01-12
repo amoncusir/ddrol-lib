3d3bec18de542cf7e6c74ec38b2446225f3c5c04
Aran Moncusí Ramírez <amoncusi@paradigmadigital.com>
2021-01-12 22:25:05 +0100

    wip

--- lib/domain/game.ts
+++ lib/domain/game.ts
@@ -1,4 +1,4 @@
-import { User } from "@domain/user";
+import { User } from "@lib/domain/user";
 
 
 export interface Game
--- lib/domain/user.ts
+++ lib/domain/user.ts
@@ -4,27 +4,26 @@ export interface UserProperties
     id: string
     email: string
     authorizedUser: boolean
-    
     additionalData: any
 }
 
 export class User implements UserProperties
 {
-    public readonly id: string;
-    public readonly email: string;
-    public readonly authorizedUser: boolean;
-    public readonly additionalData: any;
-    
-    constructor(
-        properties: UserProperties
-    )
-    {
-    
-    }
+    public constructor(
+            public readonly id: string,
+            public readonly email: string,
+            public readonly authorizedUser: boolean,
+            public readonly additionalData: any
+    ) { }
     
     static instanceWithUserProperties(properties: UserProperties)
     {
-        return new User(properties)
+        return new User(
+            properties.id,
+            properties.email,
+            properties.authorizedUser,
+            properties.additionalData
+        )
     }
 }
 
